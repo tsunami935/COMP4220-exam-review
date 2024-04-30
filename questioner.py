f41f16acf409e58d2fe520760dac90ecb53dd174
Nam <ndbui03@gmail.com>
2024-04-30 11:28:29 -0400

    improve performance w/ memoized levenshtein distance

--- questioner.py
+++ questioner.py
@@ -2,27 +2,25 @@ import random
 import sys
 import time
 import os
-import functools
 
-@functools.lru_cache
-def levenshteinRecursive(str1, str2, m, n):
-      # str1 is empty
-    if m == 0:
-        return n
-    # str2 is empty
-    if n == 0:
-        return m
-    if str1[m - 1] == str2[n - 1]:
-        return levenshteinRecursive(str1, str2, m - 1, n - 1)
-    return 1 + min(
-          # Insert     
-        levenshteinRecursive(str1, str2, m, n - 1),
-        min(
-              # Remove
-            levenshteinRecursive(str1, str2, m - 1, n),
-          # Replace
-            levenshteinRecursive(str1, str2, m - 1, n - 1))
-    )
+def levenshtein_distance(s, t):
+    m, n = len(s) + 1, len(t) + 1
+    d = [[0] * n for _ in range(m)]
+
+    for i in range(1, m):
+        d[i][0] = i
+
+    for j in range(1, n):
+        d[0][j] = j
+
+    for j in range(1, n):
+        for i in range(1, m):
+            substitution_cost = 0 if s[i - 1] == t[j - 1] else 1
+            d[i][j] = min(d[i - 1][j] + 1,
+                          d[i][j - 1] + 1,
+                          d[i - 1][j - 1] + substitution_cost)
+
+    return d[m - 1][n - 1]
 
 random.seed(time.time())
 
@@ -59,18 +57,19 @@ while finish != "N":
     question, answer = exam.pop()
     print(question)
     ans = input("Answer: ").lower().strip()
-    correct = all([True if a.lower() in answer.lower() else False for a in ans.split(',')]) if len(ans) else False
+    correct = all([True if a.lower() in answer.lower() else False for a in ans.split(' ')]) if len(ans.split(' ')) > 1 else ans.lower() == answer.lower()
     status = ""
+    avglen = (len(answer) + len(ans)) / 2
     if correct:
         status = "Correct"
         used.append((question, answer))
-    elif levenshteinRecursive(ans.lower(), answer.lower(), len(ans), len(answer)) / len(answer) < 0.3:
+    elif (len(ans) - len(answer)) / avglen < 0.25 and levenshtein_distance(ans, answer) / avglen < 0.3:
         status = "Close"
         used.append((question, answer))
     else:
         status = "Incorrect"
         exam.append((question, answer))
         random.shuffle(exam)
-    print(len(exam))
     print(f"{status}: {answer}")
+    print(f"Questions left: {len(exam)}")
     finish = input("Would you like to continue (Y/N)? ").upper()
